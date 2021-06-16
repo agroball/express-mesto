04097254794e4ba2ad5b48110b190c0ba2cc9576
Roman <rotimqw@gmail.com>
2021-06-16 16:20:03 +0300

    2

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const helmet = require('helmet');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
 
 const PORT = 3000;
@@ -16,17 +17,17 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(helmet());
 app.use((req, res, next) => {
-  req.user = {
-    _id: '60c35a78e0346243e8f8a980', // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
-  next();
+    req.user = {
+        _id: '60c35a78e0346243e8f8a980', // вставьте сюда _id созданного в предыдущем пункте пользователя
+    };
+    next();
 });
 
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on port ${PORT}`);
 });
