422dd474667479a733c2a178dc6e48392aeae625
Roman TIMAKOV <timakov_r@M11highway.pro>
2021-08-16 09:50:35 +0300

    yep

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация');
+    next(new AuthError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET_KEY);
   } catch (err) {
-    throw new AuthError('Необходима авторизация');
+    next(new AuthError('Необходима авторизация'));
   }
 
   req.user = payload;
