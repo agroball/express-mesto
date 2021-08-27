cc7f892f27bdcefd43ec6679c67633949cab1d86
Roman TIMAKOV <timakov_r@M11highway.pro>
2021-08-27 10:55:46 +0300

    for serv

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
@@ -12,8 +14,10 @@ const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/notFound');
 const { validationSignIn, validationSignUp } = require('./middlewares/validation');
 const { errorAll } = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
+app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -22,15 +26,29 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(cors({
+  origin: 'https://agroball.sharli.nomoredomains.work',
+  credentials: true,
+}));
+
+app.use(requestLogger);
 app.use(helmet());
 app.use(express.json());
 
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signup', validationSignUp, createUser);
 app.post('/signin', validationSignIn, login);
 
 app.use(auth);
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
+app.use(errorLogger);
+
 app.use(errors());
 app.use('*', (req, res, next) => next(new NotFoundError('Ресурс не найден')));
 app.use(errorAll);
